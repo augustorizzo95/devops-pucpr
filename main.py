3126e5e1281b0d03d2bc37b7d3a617bca8f8b66c
augustorizzo95 <augustojubei@gmail.com>
2026-08-30 11:43:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/helloworld")
 async def root():
     return {"message": "Hello World"}
+
+# http://127.0.0.1:8000/teste1
+@app.get("/teste1")
+async def root():
+    return {"teste": True, "num_aleatorio": random.randint(0, 10000)}
